b1782ecebe28557ac517ebe16f86b396268a0d77
Julien Legendre <jlegendre@icfo.net>
2024-05-15 19:04:56 +0200

    Update constants

--- src/miscellaneous/FundamentalConstants.m
+++ src/miscellaneous/FundamentalConstants.m
@@ -2,8 +2,8 @@
 
 global c eps0 hb kb e m0 %#ok<GVMIS> 
 c=2.99792458e8;                         % speed of light in vacuum (m/s)
-eps0=8.854187817e-12;                   % vacuum permittivity (F/m)
-hb=1.054571596e-34;                     % reduced Planck constant (J.s)
-kb=1.38064852e-23;                      % Boltzmann constant (m^2.kg/(s^2.K))
-e=1.60217733e-19;                       % elementary charge (C)
+eps0=8.8541878128e-12;                  % vacuum permittivity (F/m)
+hb=1.054571817646e-34;                  % reduced Planck constant (J.s)
+kb=1.380649e-23;                        % Boltzmann constant (m^2.kg/(s^2.K))
+e=1.602176634e-19;                      % elementary charge (C)
 m0=9.10938215e-31;                      % electron mass (kg)
